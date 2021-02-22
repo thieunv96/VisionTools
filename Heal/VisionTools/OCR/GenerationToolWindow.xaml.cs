097e12b6df3672c20ff47480a560da5d81ac88a3
My Computer <thieunv.nb@example.com>
2021-02-22 22:31:10 +0700

    init

---
+++ Heal/VisionTools/OCR/GenerationToolWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Heal.VisionTools.OCR
+{
+    /// <summary>
+    /// Interaction logic for GenerationToolWindow.xaml
+    /// </summary>
+    public partial class GenerationToolWindow : Window
+    {
+        public GenerationToolWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Heal/VisionTools/OCR/Struct/GenConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heal.VisionTools.OCR.Struct
+{
+    public class GenConfig
+    {
+        public string FontPath { get; set; }
+
+    }
+}
